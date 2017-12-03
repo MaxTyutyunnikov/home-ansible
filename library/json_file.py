14d1fa09c137c24662dcbf65a497f766cf12a45b
Sam Yaple <sam+git@yaple.net>
2017-12-03 07:22:09 -0500

    Idempotently roll_ca certs
    Still need detection of changed cert for regeneration of other certs

--- library/json_file.py
+++ library/json_file.py
@@ -15,7 +15,7 @@ def main():
             path=dict(type='path', required=True, aliases=['dest']),
             location=dict(type='str'),
             key=dict(type='str'),
-            data=dict(type='str'),
+            data=dict(type='raw'),
         ),
     )
 
@@ -24,24 +24,30 @@ def main():
     key = module.params['key']
     data = module.params['data']
 
-    with open(path, 'r') as f:
-        content = json.load(f)
+    ret = {
+        'changed': False,
+        'path': path,
+    }
 
-    if not content:
+    if os.path.isfile(path):
+        with open(path, 'r') as f:
+            content = json.load(f)
+    else:
         content = {}
+
     if location not in content:
         content[location] = {}
+
+    old_data = content[location][key] if key in content[location] else None
+    if old_data == data:
+        module.exit_json(**ret)
+
     content[location][key] = data
-
     with open(path, 'w') as f:
-        json.dump(content, f)
+        json.dump(content, f, indent=4, sort_keys=True)
+    ret['changed'] = True
 
-    results = dict(
-        changed=True,
-        path=path,
-    )
-
-    module.exit_json(**results)
+    module.exit_json(**ret)
 
 if __name__ == '__main__':
     main()
